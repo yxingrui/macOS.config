77fee470c49c2a063aa98943033d0e6687e11c0d
x1ngrui <31928784+x1ngrui@users.noreply.github.com>
2017-11-01 18:52:43 +0800

    backup_1_11_2017_18_52

--- install.sh
+++ install.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-echo 'hello,world!'
+
+# copy bash config into the home directory
+echo "copying bash_profile..."
+cp ./bash_config/bash_profile ~/.bash_profile
+source ~/.bash_profile
+
+echo "job done"
