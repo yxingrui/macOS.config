d196ec31fed606bf128a4e4431fa27cfa0918d30
x1ngrui <31928784+x1ngrui@users.noreply.github.com>
2017-11-02 19:50:02 +0800

    modified:   backup.sh 	modified:   install.sh

--- backup.sh
+++ backup.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 echo 'copying bash_profile...'
 cp ~/.bash_profile ./bash_config/bash_profile
+
 echo 'copying .emacs...'
 cp ~/.emacs ./emacs_config/emacs
+
 echo 'copying .emacs.d...'
 cp -R ~/.emacs.d/ ./emacs_config/emacs.d/
+
+echo 'INFO: Pushing to GitHub...'
+git commit -m $(date +'backup_%d_%m_%y_%H_%M')
+git push
+
 echo 'done'
--- install.sh
+++ install.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
 # copy bash config into the home directory
-echo "copying bash_profile..."
+echo "INFO: Copying bash_profile..."
 cp ./bash_config/bash_profile ~/.bash_profile
 source ~/.bash_profile
+
+# copy emacs config files into the home directory
+echo "INFO: Copying .emacs.d..."
+
+if [[ ! -d '~/.emacs.d/' && -e '~/.emacs.d/' ]]; then
+    echo 'INFO: .emacs.d dose not exist, creating...'
+    mkdir ~/.emacs.d
+fi
+cp -R ./emacs_config/emacs.d/ ~/.emacs.d
 
 echo "job done"
